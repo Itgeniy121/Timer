54cfb9271a2943aa7b36fefe3523a2605514e419
Aleksey <120134806+Itgeniy121@users.noreply.github.com>
2022-12-10 13:04:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let Hours = document.querySelector('.Hours');
 let Minutes = document.querySelector('.Minutes');
 let Seconds = document.querySelector('.Seconds');
 let Milliseconds = document.querySelector('.Milliseconds');
+//styles
+let back = document.querySelectorAll('.timerblock');
+let TitleHours = document.querySelector('.hours');
+let TitleMinutes = document.querySelector('.minutes');
+let TitleSeconds = document.querySelector('.seconds');
+let titleMilliseconds = document.querySelector('.milliseconds');
 
 //ForLaps
 let TITLE = document.querySelector('.TITLE');
@@ -15,6 +21,7 @@ let start = document.querySelector('.start');
 let pause = document.querySelector('.pause');
 let lap = document.querySelector('.lap');
 let restart = document.querySelector('.restart');
+let night = document.querySelector('.nighttheme');
 
 //listeners
 start.addEventListener('click', () => {
@@ -24,6 +31,8 @@ start.addEventListener('click', () => {
 restart.addEventListener('click', RestaerTime);
 pause.addEventListener('click', Pausetime);
 lap.addEventListener('click', LapTime);
+night.addEventListener('click', NightTheme);
+
 
 
 
@@ -105,10 +114,67 @@ function LapTime(){
     Ol.appendChild(NewDiv);
     NewDiv.className = 'crug';
     NewDiv.innerHTML = `${HOURS} : ${MINUTES} : ${SECONDS} : ${MILLISECONDS}`;
-};
+}
+
 function Pausetime(){
     clearInterval(interval);
 }
+
+
+let flag = 1;
+function NightTheme(){
+    if(flag ==1){
+    document.getElementsByTagName('body')[0].style = 'background: #1C0E3A';
+    TitleHours.style = 'background-color: #1E0B46';
+    TitleMinutes.style = 'background-color: #1E0B46';
+    TitleSeconds.style = 'background-color: #1E0B46';
+    titleMilliseconds.style = 'background-color: #1E0B46';
+    start.classList.add('selected1');
+    pause.classList.add('selected2');
+    restart.classList.add('selected3');
+    lap.classList.add('selected4');
+    start.classList.remove('start');
+    pause.classList.remove('pause');
+    restart.classList.remove('restart');
+    lap.classList.remove('lap');
+    night.innerHTML = '☀️';
+    night.style = 'background: #7B68EE';
+
+    for(let elem of back){
+        elem.style = 'background-color: #29105D';
+        elem.style.border = '1px solid #29105D';
+    }
+    flag = 2;
+} else  {
+
+    for(let elem of back){
+        elem.style = 'background-color: blueviolet';
+        elem.style.border = '1px solid blueviolet';
+    }
+
+    document.getElementsByTagName('body')[0].style = 'background: #7B68EE';
+    TitleHours.style = 'background-color: #4B0082';
+    TitleMinutes.style = 'background-color: #4B0082';
+    TitleSeconds.style = 'background-color: #4B0082';
+    titleMilliseconds.style = 'background-color: #4B0082';
+    start.classList.remove('selected1');
+    pause.classList.remove('selected2');
+    restart.classList.remove('selected3');
+    lap.classList.remove('selected4');
+    start.classList.add('start');
+    pause.classList.add('pause');
+    restart.classList.add('restart');
+    lap.classList.add('lap');
+    night.innerHTML = '🌙';
+    night.style = 'background: #2B0F66';
+    flag = 1;
+
+}
+
+
+
+}
+
 
 
 
